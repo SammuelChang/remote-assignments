ec7706c8e397cc44e5273b5c4cea7fbd95550661
SammuelChang <88440022+SammuelChang@users.noreply.github.com>
2022-03-30 14:19:26 +0800

    Create app.js

---
+++ week-2/assignment1/app.js
@@ -0,0 +1,9 @@
+function max(numbers){
+let maxNumber = 0;
+for ( let i = 0; i < numbers.length; i++){
+        if (numbers[i] > maxNumber) {
+            maxNumber = numbers[i];
+        }
+    }
+return maxNumber;
+}
