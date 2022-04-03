4b6cb99fd4e16bdc561e318360887449e9126233
SammuelChang <88440022+SammuelChang@users.noreply.github.com>
2022-04-03 17:04:29 +0800

    Update app.js

--- week-2/assignment1/app.js
+++ week-2/assignment1/app.js
@@ -1,5 +1,7 @@
+// 2022-04-03 將maxNumber從0修正為-50000，避免輸入陣列包含負數時，最大值判斷失準
+
 function max(numbers){
-let maxNumber = 0;
+let maxNumber = -50000;
 for ( let i = 0; i < numbers.length; i++){
         if (numbers[i] > maxNumber) {
             maxNumber = numbers[i];
