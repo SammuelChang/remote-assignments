7d2146af9dac175688a7adffb0011fa09fdd643d
SammuelChang <88440022+SammuelChang@users.noreply.github.com>
2022-04-03 17:31:58 +0800

    Update app.js

--- week-2/assignment5/app.js
+++ week-2/assignment5/app.js
@@ -1,5 +1,6 @@
-//2022-04-03 將迴圈內層加入break條件
+//2022-04-03 將迴圈加入break條件
 
+//原寫法
 function twoSum(nums, target){
     // your code here
     let index = [];
@@ -7,15 +8,16 @@ function twoSum(nums, target){
         for ( let j = 0; j < nums.length; j++){
             if ( nums[i] !== nums[j] && (nums[i] + nums[j]) === target && i < j){
                 index = [i,j];
-                break;
-                console.log(`${i},${j}`); //測試有無正確break
             }
+           if (index.length > 0){break}
         }
+        if (index.length > 0){break}
     }
     console.log(index)
     }
 
     twoSum([2, 7, 11, 15], 9)
+
     /*
     For example:
         twoSum([2, 7, 11, 15], 9); 
